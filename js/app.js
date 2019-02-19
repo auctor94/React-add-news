412cffb36a7685cb99f60e144c7049ade6885a07
Tima Andron <timashenka99@gmail.com>
2019-02-19 19:48:09 +0300

    FAQ

--- js/app.js
+++ js/app.js
@@ -1,27 +1,58 @@
 var my_news = [
     {
-        author: 'Саша Печкин',
-        text: 'В четверг, четвертого числа...'
+        author: 'Timofei Andronovich',
+        text: 'Перед собеседованием по JS...',
+        bigText: 'я открыл книгу по React и стал всё вспоминать.'
+        },
+    {
+    author: 'Саша Печкин',
+    text: 'В четчерг, четвертого числа...',
+    bigText: 'в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж.'
     },
     {
-        author: 'Просто Вася',
-        text: 'Считаю, что $ должен стоить 35 рублей!'
+    author: 'Просто Вася',
+    text: 'Считаю, что $ должен стоить 35 рублей!',
+    bigText: 'А евро 42!'
     },
     {
-        author: 'Гость',
-        text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+    author: 'Гость',
+    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
+    bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
     }
-];
+    ];
 
 var Article = React.createClass({
+    propTypes: {
+        data: React.PropTypes.shape({
+            author: React.PropTypes.string.isRequired,
+            text: React.PropTypes.string.isRequired,
+            bigText: React.PropTypes.string.isRequired
+        })
+    },
+    getInitialState: function () {
+        return {
+            visible: false
+        };
+    },
+    readmoreClick: function (e) {
+        e.preventDefault();
+        this.setState({ visible: true });
+    },
     render: function () {
         var author = this.props.data.author,
-            text = this.props.data.text;
+            text = this.props.data.text,
+            bigText = this.props.data.bigText,
+            visible = this.state.visible;
 
         return (
             <div className="article">
                 <p className="news__author">{author}</p>
                 <p className="news__text">{text}</p>
+                <a href="#" onClick={this.readmoreClick} 
+                className={'news__readmore ' + (visible ? 'none': '')}>
+                Подробнее
+                </a>
+                <p className={'news__big-text ' + (visible ? '': 'none')}>{bigText}</p>
             </div>
         )
     }
@@ -29,6 +60,9 @@ var Article = React.createClass({
 
 
 var News = React.createClass({
+    propTypes: {
+        data: React.PropTypes.array.isRequired
+    },
     render: function () {
         var data = this.props.data;
         var newsTemplate;
@@ -54,12 +88,106 @@ var News = React.createClass({
 });
 
 
+var Add = React.createClass({
+    getInitialState: function () { //устанавливаем начальное состояние (state)
+        return {
+            agreeNotChecked: true,
+            authorIsEmpty: true,
+            textIsEmpty: true
+        };
+    },
+    componentDidMount: function () { //ставим фокус в input
+        ReactDOM.findDOMNode(this.refs.author).focus();
+    },
+    onBtnClickHandler: function (e) {
+        e.preventDefault();
+        var textEl = ReactDOM.findDOMNode(this.refs.text);
+        
+        var author = ReactDOM.findDOMNode(this.refs.author).value;
+        var text = textEl.value;
+
+        var item = [{
+            author: author,
+            text: text,
+            bigText: '...'
+        }];
+
+        window.ee.emit('News.add', item);
+
+        textEl.value = '';
+        this.setState({textIsEmpty: true});
+    },
+    onCheckRuleClick: function (e) {
+        this.setState({ agreeNotChecked: !this.state.agreeNotChecked }); //устанавливаем значение в state
+    },
+    onFieldChange: function (fieldName, e) {
+        if (e.target.value.trim().length > 0) {
+            this.setState({ [''+fieldName]: false })
+        } else {
+            this.setState({ [''+fieldName]: true })
+        }
+    },
+    render: function () {
+        var agreeNotChecked = this.state.agreeNotChecked,
+            authorIsEmpty = this.state.authorIsEmpty,
+            textIsEmpty = this.state.textIsEmpty;
+        return (
+            <form className='add cf'>
+                <input
+                    type='text'
+                    className='add__author'
+                    onChange={this.onFieldChange.bind(this, 'authorIsEmpty')}
+                    placeholder='Ваше имя'
+                    ref='author'
+                />
+                <textarea
+                    className='add__text'
+                    onChange={this.onFieldChange.bind(this, 'textIsEmpty')}
+                    placeholder='Текст новости'
+                    ref='text'
+                ></textarea>
+                <label className='add__checkrule'>
+                    <input type='checkbox' ref='checkrule' onChange={this.onCheckRuleClick}/>Я согласен с
+                    правилами
+                </label>
+                <button
+                    className='add__btn'
+                    onClick={this.onBtnClickHandler}
+                    ref='alert_button'
+                    disabled={agreeNotChecked || authorIsEmpty || textIsEmpty}>
+                    Добавить новость
+                </button>
+            </form>
+        );
+    }
+});
+//Глобальная переменная, чтобы отлавливать события в обработчиках
+window.ee = new EventEmitter();
+
+
+
 var App = React.createClass({
+    getInitialState: function () {
+        return {
+            news: my_news
+        };
+    },
+    componentDidMount: function () {
+        var self = this;//сохраняем this для collback`a
+        window.ee.addListener('News.add', function (item) {
+            var nextNews = item.concat(self.state.news);//создали массив, в котором первым элементом поставили новую новость
+            self.setState({ news: nextNews });
+        });
+    },
+    componentWillUnmount: function () {
+        window.ee.removeListener('News.add');
+    },
     render: function () {
         return (
             <div className="app">
                 <h3>Новости</h3>
-                <News data={my_news} />
+                <Add />
+                <News data={this.state.news} />
             </div>
         );
     }
